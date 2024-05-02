e9e36b5d2318eef541ea60514a5cad2ae3ef4508
LibeUwU <159199101+LibeUwU16@users.noreply.github.com>
2024-05-02 17:36:57 -0500

    Add files via upload

---
+++ EVA3_9_ARREGLOS/src/eva3_9_arreglos/EVA3_9_ARREGLOS.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_9_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_9_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String díasSemana[] = {"Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"};
+        char vocales[] = {'a', 'e', 'i', 'o', 'u'};
+        System.out.println("Tamaño del arregló díaSemana = " + díasSemana.length);
+        System.out.println("Tamaño del arregl+o vocales = " + vocales.length);
+        
+        for (int i = 0; i < díasSemana.length; i++){
+            System.out.println(díasSemana[i]);
+        }
+        for (int i = 0; i < vocales.length; i++){
+            System.out.println(vocales[i]);
+        }
+        String menú[] = {"Archivo", "Nueva Factura", "Modificar", "Salir"};
+        for (int i = 0; i < menú.length; i++){
+            System.out.println(menú[i]);
+        {
+      }
+     }
+    } 
+   }
+
